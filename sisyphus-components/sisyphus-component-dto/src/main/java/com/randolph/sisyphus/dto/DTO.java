8ec890668b2ed043daa481063ff97e5a6ac040ac
Randolph-Lu <randolph_lu@163.com>
2024-10-01 22:27:22 +0800

    feat: add DTO(Data Transfer Object)

---
+++ sisyphus-components/sisyphus-component-dto/src/main/java/com/randolph/sisyphus/dto/DTO.java
@@ -0,0 +1,13 @@
+package com.randolph.sisyphus.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @author : randolph
+ * date : 2024/10/1 20:08
+ */
+public abstract class DTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1;
+}
